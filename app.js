437700db87047b819cb0b4633a2d64f93a568f5d
Shun <shunsuke.takasu.job@gmail.com>
2024-08-16 20:12:57 +0900

    Add : Emotion Recognition

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
-var express = require("express");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var serverRouter = require('./routes/server');
+
 var app = express();
 
-const port = process.env.PORT || 8000
-var server = app.listen(port, function(){
-    console.log("Node.js is listening to PORT: http://localhost:" + server.address().port + "/server");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb'}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/server', serverRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-var text = "GET Request to the Express.js server";
-app.get("/server", function(req, res, next){
-    res.send({ message: text});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+app.listen(8000, () => {
+  console.log("running...");
+});
+
+module.exports = app;
